1fcf90b107ba63a999195885b0f7ebc8f91390f4
VitriSnake <staff.celtiumc@gmail.Com>
2022-10-25 09:39:43 +0200

    Move setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    description="Un tutoriel pas comme les autres..",
+    author="VitriSnake",
+    author_email="vitrisnake@gmail.com",
+    name="PyCrossTheAge",
+    packages=["pycrosstheage"],
+    url="https://matrix.to/#/@vitrisnake-dev:matrix.org",
+    version="0.1.0",
+)
